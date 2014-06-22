16c45fbcabbdedee5acf50df180122c3e02159c2
weibaohui <zihuxinyu@163.com>
2014-06-22 23:32:02 +0800

    js

---
+++ assets/js/scripts/boot.js
@@ -0,0 +1,139 @@
+﻿__CreateJSPath = function (js) {
+    var scripts = document.getElementsByTagName("script");
+    var path = "";
+    for (var i = 0, l = scripts.length; i < l; i++) {
+        var src = scripts[i].src;
+        if (src.indexOf(js) != -1) {
+            var ss = src.split(js);
+            path = ss[0];
+            break;
+        }
+    }
+    var href = location.href;
+    href = href.split("#")[0];
+    href = href.split("?")[0];
+    var ss = href.split("/");
+    ss.length = ss.length - 1;
+    href = ss.join("/");
+    if (path.indexOf("https:") == -1 && path.indexOf("http:") == -1 && path.indexOf("file:") == -1 && path.indexOf("\/") != 0) {
+        path = href + "/" + path;
+    }
+    return path;
+}
+
+var bootPATH = __CreateJSPath("boot.js");
+
+//debugger
+mini_debugger = true;
+
+//miniui
+document.write('<script src="' + bootPATH + 'jquery-1.6.2.min.js" type="text/javascript"></sc' + 'ript>');
+document.write('<script src="' + bootPATH + 'miniui/miniui.js" type="text/javascript" ></sc' + 'ript>');
+document.write('<link href="' + bootPATH + 'miniui/themes/default/miniui.css" rel="stylesheet" type="text/css" />');
+document.write('<link href="' + bootPATH + 'miniui/themes/icons.css" rel="stylesheet" type="text/css" />');
+
+//skin
+var skin = getCookie("miniuiSkin");
+if (skin) {
+    document.write('<link href="' + bootPATH + 'miniui/themes/' + skin + '/skin.css" rel="stylesheet" type="text/css" />');
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+function getCookie(sName) {
+    var aCookie = document.cookie.split("; ");
+    var lastMatch = null;
+    for (var i = 0; i < aCookie.length; i++) {
+        var aCrumb = aCookie[i].split("=");
+        if (sName == aCrumb[0]) {
+            lastMatch = aCrumb;
+        }
+    }
+    if (lastMatch) {
+        var v = lastMatch[1];
+        if (v === undefined) return v;
+        return unescape(v);
+    }
+    return null;
+}
+
+
+function GetParams(url, c) {
+    if (!url) url = location.href;
+    if (!c) c = "?";
+    url = url.split(c)[1];
+    var params = {};
+    if (url) {
+        var us = url.split("&");
+        for (var i = 0, l = us.length; i < l; i++) {
+            var ps = us[i].split("=");
+            params[ps[0]] = decodeURIComponent(ps[1]);
+        }
+    }
+    return params;
+}
+
+
+function onSkinChange(skin) {
+    //mini.Cookie.set('miniuiSkin', skin);
+    mini.Cookie.set('miniuiSkin', skin, 100);//100天过期的话，可以保持皮肤切换
+    window.location.reload()
+}
+function AddCSSLink(id, url, doc) {
+    doc = doc || document;
+    var link = doc.createElement("link");
+    link.id = id;
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("type", "text/css");
+    link.setAttribute("href", url);
+
+    var heads = doc.getElementsByTagName("head");
+    if (heads.length)
+        heads[0].appendChild(link);
+    else
+        doc.documentElement.appendChild(link);
+}
+function alert(str) {
+    mini.showTips({
+        content: "<b>" + str + "</b> ",
+        state: "warning",
+        x: "center",
+        y: "top",
+        timeout: 1000
+    });
+}
+
+/***********grid op start***********/
+
+function addRow(htmlguidid) {
+    var grid = mini.get(htmlguidid);
+    grid.addRow({}, 0);
+    grid.beginEditCell({}, 0);
+}
+function removeRow(htmlguidid) {
+    var grid = mini.get(htmlguidid);
+    var rows = grid.getSelecteds();
+    if (rows.length > 0) {
+        grid.removeRows(rows, true);
+    }
+}
+function saveGrid(htmlguidid, posturl) {
+    var grid = mini.get(htmlguidid);
+    var data = grid.getChanges(null, true);
+    var json = mini.encode(data);
+    if (json.length > 0) {
+        grid.loading("保存中，请稍后......");
+        $.ajax({
+            url: posturl,
+            data: { data: json },
+            type: "post",
+            success: function (text) {
+                grid.reload();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(jqXHR.responseText);
+            }
+        });
+    }
+}
+/***********grid op end***********/
